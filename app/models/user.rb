2467d1df9ff617f061e9b136a37f8601ffd060d5
Larissa Reis <larissa@colivre.coop.br>
2014-11-26 01:13:49 -0300

    Fix cpf validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   validates :username, :first_name, :last_name, :cpf, :birth, :gender, :email, presence: true
   validates :gender, inclusion: { in: %w( F M O ) }
   validates :fist_name, :last_name, length: { maximum: 30 }
-  validates :cpf, format: { with: /^(\d{3}\.?\d{3}\.?\d{3})-?(\d{2})$/i, on: :create }
+  validates :cpf, format: { with: /\A(\d{3}\.?\d{3}\.?\d{3})-?(\d{2})\Z/i, on: :create }
   validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
   validates :username, uniqueness: { case_sensitive: false }, format: { with: /\A[-a-z0-9]+\Z/i, on: :create }
 end
