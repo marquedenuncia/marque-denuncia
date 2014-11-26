9c6681f20adffa9424dca49d466fbe13631a2a65
Larissa Reis <larissa@colivre.coop.br>
2014-11-26 01:08:40 -0300

    Includes more user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   validates :username, :first_name, :last_name, :cpf, :birth, :gender, :email, presence: true
   validates :gender, inclusion: { in: %w( F M O ) }
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
-  validates :cpf, format: { with: /^(\d{3}\.?\d{3}\.?\d{3})-?(\d{2})$/i, on: :create }
   validates :fist_name, :last_name, length: { maximum: 30 }
-  validates :username, uniqueness: true
+  validates :cpf, format: { with: /^(\d{3}\.?\d{3}\.?\d{3})-?(\d{2})$/i, on: :create }
+  validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+  validates :username, uniqueness: { case_sensitive: false }, format: { with: /^[-a-z0-9]+$/i, on: :create }
 end
