5e59490de24baf50e53e398a1bb840f8b83d2587
Ícaro Jerry <icaro.jerry@gmail.com>
2014-11-27 00:39:40 -0300

    Fixes seeds that exceeds the maximum string size

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@
 
 User.create(username: 'admin', first_name: 'Icaro', last_name: 'Jerry', birth: Date.today, gender: 'M', cpf: '000.000.000-00', admin: true, email: 'test1@localhost.localdomain', password: '11111111', password_confirmation: '11111111')
 
-incident_types = ['Lesão Corporal', 'Estupro', 'Destruição de bens e documentos',
+incident_types = ['Lesão Corporal', 'Estupro', 'Destruição de bens/documentos',
                   'Calúnia, difamação e injúria', 'Assédio Sexual',
                   'Racismo e injúria racial', 'Lesbofobia', 'Assédio Moral',
-                  'Ameaça', 'Abuso Sexual', 'Aborto provocado por outra pessoa',
+                  'Ameaça', 'Abuso Sexual', 'Aborto provocado por outros',
                   'Encoxadas', 'Encoxadas', 'Exploração Sexual', 'Cárcere Privado',
-                  'Tráfico de mulheres para fins de exploração sexual', 'Trabalho escravo']
+                  'Tráfico de mulheres', 'Trabalho escravo']
 
 incident_types.each { |name| IncidentType.create(description: name) }
