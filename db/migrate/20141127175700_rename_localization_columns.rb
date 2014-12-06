71779a354b65ed1ef81b493d78b2a215b36252a9
Larissa Reis <larissa@colivre.coop.br>
2014-12-05 23:38:41 -0300

    Add copyright text to source files

--- db/migrate/20141127175700_rename_localization_columns.rb
+++ db/migrate/20141127175700_rename_localization_columns.rb
@@ -1,3 +1,15 @@
+# Copyright 2014 de Larissa Reis e Icaro Jerry
+#
+# Este arquivo é parte do programa Marque Denúncia. O Marque Denúncia é um
+# software livre; você pode redistribuí-lo e/ou modificá-lo dentro dos termos da
+# GNU General Public License como publicada pela Fundação do Software Livre
+# (FSF); na versão 3 da Licença. Este programa é distribuído na esperança que
+# possa ser útil, mas SEM NENHUMA GARANTIA; sem uma garantia implícita de
+# ADEQUAÇÃO a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a licença para
+# maiores detalhes. Você deve ter recebido uma cópia da GNU General Public
+# License, sob o título LICENCA, junto com este programa, se não, acesse
+# http://www.gnu.org/licenses/
+
 class RenameLocalizationColumns < ActiveRecord::Migration
   def change
     rename_column :establishments, :lat, :latitude
